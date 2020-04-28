9f02683a5681ae03495d0b966bae4696bee838e3
Caleb Isaac Mora Díaz <calexico659@gmail.com>
2020-04-28 18:59:50 -0500

    Navbar, Footer, Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/foorter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent, NavbarComponent, FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/shared/footer/foorter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-footer',
+    templateUrl: './foorter.component.html',
+})
+export class FooterComponent {
+    constructor() { }
+}
